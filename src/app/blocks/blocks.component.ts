0fb25070c5e6de9295a63aca1ec14e3285d295b5
aakatev <aakatev@mail.ccsf.edu>
2019-01-29 13:01:48 -0800

    Added feature to browse older blocks

--- src/app/blocks/blocks.component.ts
+++ src/app/blocks/blocks.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Block } from './block';
 
 @Component({
   selector: 'app-blocks',
@@ -8,19 +7,38 @@ import { Block } from './block';
   styleUrls: ['./blocks.component.css']
 })
 export class BlocksComponent implements OnInit {
+  currentBlock = 0;
+  startBlock = 0;
   blocks = [];
+  blocksToDisplay = 20;
 
   constructor(private http: HttpClient) { }
 
+  fetchBlocks() {
+    this.http.get(`https://aakatev.me/iris/blockchain?minHeight=${this.currentBlock-this.blocksToDisplay+1}&maxHeight=${this.currentBlock}`).subscribe(data => {
+      this.blocks = data['result'].block_metas;
+    });
+  }
+
+  displayOlderBlocks() {
+    if(this.currentBlock - this.blocksToDisplay > 20) {
+      this.currentBlock -= this.blocksToDisplay;
+    }
+    this.fetchBlocks();
+  }
+
+  displayNewerBlocks() {
+    if(this.currentBlock + this.blocksToDisplay <= this.startBlock) {
+      this.currentBlock += this.blocksToDisplay;
+    }
+    this.fetchBlocks();
+  }
+
   ngOnInit() {
     this.http.get('https://aakatev.me/iris/status').subscribe(data => {
-      // Debugging
-      // let currValidators = data['result'].genesis.validators;
-      let lastBlock = data['result'].sync_info.latest_block_height;
-
-      this.http.get(`https://aakatev.me/iris/blockchain?minHeight=${lastBlock-50}&maxHeight=${lastBlock}`).subscribe(data => {
-        this.blocks = data['result'].block_metas;
-      });
+      this.startBlock = data['result'].sync_info.latest_block_height;
+      this.currentBlock = this.startBlock;
+      this.fetchBlocks();
     });
   }
 }
