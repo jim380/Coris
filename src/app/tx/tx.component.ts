a11fe25155000c043bed1b0587eb68e4c65efa80
aakatev <aakatev@mail.sfsu.edu>
2019-03-25 13:49:17 +0000

    Query txs by hash feature added to tx component too

--- src/app/tx/tx.component.ts
+++ src/app/tx/tx.component.ts
@@ -4,7 +4,7 @@ import { Inject }  from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { nodeRpc } from '../../config.js'
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tx',
@@ -15,13 +15,35 @@ export class TxComponent implements OnInit {
   txHash;
   tx = {};
 
-  constructor(private http: HttpClient, @Inject(DOCUMENT) document, private route: ActivatedRoute) {  
+  async clickButton(value) {
+    // Debugging @aakatev
+    // console.log(value);
+    await this.router.navigate([`tx/${value}`]);
+    this.queryTx();
+  }
+
+  initTxHash() {
     this.txHash = this.route.snapshot.paramMap.get('hash');
   }
 
-  ngOnInit() {
+  constructor(
+    private http: HttpClient, 
+    @Inject(DOCUMENT) document, 
+    private route: ActivatedRoute, 
+    private router: Router ) {  
+      this.initTxHash();
+  }
+
+  queryTx () {
+    this.initTxHash();
     this.http.get(`${nodeRpc}/tx_search?query="tx.hash='${this.txHash}'"`).subscribe(data => {
       if (data['error'] === undefined) this.tx = data['result'].txs[0];
+      else this.tx = {};
     });
+    console.log(this.tx);
+  }
+
+  ngOnInit() {
+    this.queryTx();
   }
 }
