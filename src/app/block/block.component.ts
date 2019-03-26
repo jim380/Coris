a30338c8d9e48124a769f62ef749f9e6d8fd1b2c
aakatev <aakatev@mail.ccsf.edu>
2019-03-26 00:04:37 -0700

    Blocks query by height added

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common'; 
 import { Inject }  from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { nodeRpc } from '../../config.js'
+
 
 @Component({
   selector: 'app-block',
@@ -16,17 +17,24 @@ export class BlockComponent implements OnInit {
   queryHeight = 0;
   displayCommits = false;
 
-  constructor(private http: HttpClient, @Inject(DOCUMENT) document, private route: ActivatedRoute) { 
-    this.queryHeight = Number(this.route.snapshot.paramMap.get('height'));
+  constructor(private http: HttpClient, @Inject(DOCUMENT) document, private route: ActivatedRoute, private router: Router) { 
   }
 
+  async clickButton(value) {
+    await this.router.navigate([`block/${value}`]);
+
+    this.fetchBlock();
+  }
   ngOnInit() {
     this.fetchBlock();
   }
 
   fetchBlock() {
+    this.queryHeight = Number(this.route.snapshot.paramMap.get('height'));
+
     this.http.get(`${nodeRpc}//block?height=${this.queryHeight}`).subscribe(data => {
-      this.block = data['result'].block;
+      if (data['error'] === undefined) this.block = data['result'].block;
+      else this.block = {};
     });
   }
 
