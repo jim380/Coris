457c6d40e19bc15f8b8f7df983b39b6eb5afc740
jim380 <jyz1992@hotmail.com>
2019-04-06 02:03:17 -0700

    added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,38 @@ import { WsService } from './services/ws.service';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { ValidatorsService } from './services/validators.service';
+import { trigger, state, query, transition, animate, style, keyframes, animation, useAnimation, animateChild, group, stagger } from '@angular/animations';
+import { fadeInAnimation, fade } from './animations/animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('dropDownAnimation', [
+      transition(':enter',[
+        group([
+          query('h1, h2', [
+            style({ transform: 'translateY(-20px)'}),
+            animate(1000)
+          ]),
+          query('@fadeInAnimation', 
+            stagger(200, [ animateChild() ])
+          )
+        ])
+      ])
+    ]),
+    trigger('fadeInAnimation',[
+      transition(':enter', [
+        useAnimation(fadeInAnimation, {
+          params: {
+            duration: '1500ms'
+          }
+        })
+      ])
+    ]),
+    fade
+  ]
 })
 export class AppComponent {
   appState: Observable<{blocks:[], txs:[], validators:[], round:{}, roundStep: {}, valsMap: Map<string,string>}>;
