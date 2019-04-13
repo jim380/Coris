3a50f643e750b5604fb6c56f556ff2206e233ef4
aakatev <aakatev@mail.ccsf.edu>
2019-04-13 16:36:07 -0700

    Typo in config fixed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const nodeRpc1 = 'https://aakatev.me/node_test';
+export const nodeRpc1 = 'https://aakatev.me/node_txs';
 export const nodeRpc2 = 'https://aakatev.me/node';
 export const nodeWs = 'wss://aakatev.me/node/websocket';
