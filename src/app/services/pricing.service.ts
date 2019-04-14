f3db253e2951518b1856380604ab77dcf2119af7
jim380 <jyz1992@hotmail.com>
2019-04-14 02:58:00 -0700

    placeholders added a new row of queries. Added pricing service.

---
+++ src/app/services/pricing.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+
+// see reference: https://coinmarketcap.com/api/documentation/v1/
+export class PricingService {
+
+    results: any;
+    constructor(private httpClient: HttpClient) {}
+
+    getPrice() {
+      this.httpClient.get<any>('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info', {
+        observe: 'body',
+        responseType: 'json',
+        headers: {
+            'X-CMC_PRO_API_KEY': '' // API key removed for obv reasons
+        },
+        params: new HttpParams().set('symbol', 'ATOM')
+      })
+        .subscribe(
+          (response: any) => {
+            // console.log(response);
+            return this.results = response;
+          }
+        );
+    }
+}
