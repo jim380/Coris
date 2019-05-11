28570baed6fc833c691080ca6790670e33cdf3d0
aakatev <aakatev@mail.ccsf.edu>
2019-05-11 11:09:47 -0700

    Pipeline to inject data in chart components

--- src/app/components/charts/chart-cards/chart-cards.component.ts
+++ src/app/components/charts/chart-cards/chart-cards.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { State } from 'src/app/interfaces/state.interface';
+import { Store } from '@ngrx/store';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chart-cards',
@@ -8,13 +12,13 @@ import { Component, OnInit } from '@angular/core';
 export class ChartCardsComponent implements OnInit {
   public map: any = { lat: 51.678418, lng: 7.809007 };
 
-  public chart3Type = 'line';
-  public chart2Type = 'bar';
-  public chart1Type = 'radar';
+  public blockChartType = 'line';
+  public commissionChartType = 'bar';
+  public radarChartType = 'radar';
 
-  public chart1Datasets: Array<any> = [65, 59, 40, 87];
+  public radarChartDatasets: Array<any> = [65, 59, 40, 87];
 
-  public chart2Datasets: Array<any> = [
+  public commissionChartDatasets: Array<any> = [
     {
       data: [65, 59, 80, 90],
       labels: ['January', 'Febuary', 'March', 'April'],
@@ -22,7 +26,7 @@ export class ChartCardsComponent implements OnInit {
     },
   ];
 
-  public chart3Datasets: Array<any> = [
+  public blockChartDatasets: Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: '#1'},
     {data: [28, 48, 10, 69, 36, 37, 110], label: '#2'},
     {data: [38, 58, 30, 79, 26, 37, 20], label: '#3'},
@@ -31,13 +35,13 @@ export class ChartCardsComponent implements OnInit {
 
   public chartLabels: Array<any> = ['Jan', 'Feb', 'Mar', 'Apr'];
 
-  public chart1Colors: Array<any> = [
+  public radarChartColors: Array<any> = [
     {
       backgroundColor: ['#4285F4', '#ffbb33', '#29b6f6', '#FF5252'],
       hoverBackgroundColor: ['#6ea0f2', '#fec451', '#52c3f6', '#fa6e6e']
     }
   ];
-  public chart2Colors: Array<any> = [
+  public commissionChartColors: Array<any> = [
     {
         backgroundColor: 'rgba(255,255,255,0.2)',
         borderColor: 'rgba(255,255,255,1)',
@@ -48,7 +52,7 @@ export class ChartCardsComponent implements OnInit {
         pointHoverBorderColor: 'rgba(255,255,255,1)'
     }
   ];
-  public chart3Colors: Array<any> = [
+  public blockChartColors: Array<any> = [
     {
         backgroundColor: 'rgba(220,220,220,0.2)',
         borderColor: 'rgba(220,220,220,1)',
@@ -60,7 +64,7 @@ export class ChartCardsComponent implements OnInit {
     }
   ];
 
-  public chart1Options: any = {
+  public radarChartOptions: any = {
     responsive: true,
     legend: {
       labels: {
@@ -69,7 +73,7 @@ export class ChartCardsComponent implements OnInit {
     }
   };
 
-  public chart2Options: any = {
+  public commissionChartOptions: any = {
     responsive: true,
     legend: {
       labels: {
@@ -90,9 +94,32 @@ export class ChartCardsComponent implements OnInit {
     }
   };
 
-  constructor() {
+
+  appState: Observable<State>;
+  storeSubscription$;
+
+  constructor(private store: Store <State>) {
+    this.appState = this.store.select('App');
   }
 
   ngOnInit() {
+    this.storeSubscription$ = this.appState
+      .pipe(
+        // debounceTime(3000),
+        distinctUntilChanged()
+      )
+      .subscribe(data => {
+        if(data.validators.serviceLoaded && this.storeSubscription$) {
+          // TODO remove debugging
+          console.log(data);
+          this.storeSubscription$.unsubscribe();
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    if(this.storeSubscription$) {
+      this.storeSubscription$.unsubscribe();
+    }
   }
 }
