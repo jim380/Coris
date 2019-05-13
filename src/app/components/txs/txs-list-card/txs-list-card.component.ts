f5b8e52977e80670472023439500fbe9ef585ea4
aakatev <aakatev@mail.sfsu.edu>
2019-05-12 20:05:13 -0700

    Delegator txs popup fully hooked up

--- src/app/components/txs/txs-list-card/txs-list-card.component.ts
+++ src/app/components/txs/txs-list-card/txs-list-card.component.ts
@@ -1,52 +1,62 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit, Inject, ViewChild, AfterViewInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatPaginator, MatTableDataSource } from '@angular/material';
 import { TxsService } from 'src/app/services/txs.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-txs-list-card',
   templateUrl: './txs-list-card.component.html',
   styleUrls: ['./txs-list-card.component.scss']
 })
-export class TxsListCardComponent implements OnInit {
-  address: string;
+export class TxsListCardComponent implements OnInit, AfterViewInit {
+  delegator = {
+    address: "",
+    txs: {
+      staking: [],
+      transfer: []
+    }
+  }
+  displayedColumns: string[] = ['hash', 'height'];
+  dataSource = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ts: TxsService) { 
-    this.address = data.address;
+    this.delegator.address = data.address;
     // TODO remove debugging
-    console.log(data);
+    // console.log(data);
+    
   }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
   
   ngOnInit() {
-    this.ts.getTxs(this.address, 20, 1).subscribe((data: any) => {
+    this.ts.getTxs(this.delegator.address, 20, 1).subscribe((data: any) => {
+      // TODO remove debugging
       console.log(data);
+      this.delegator.txs.transfer = data;
     });
 
-    this.ts.getStakingTxs(this.address).subscribe((data: any) => {
+    this.ts.getStakingTxs(this.delegator.address).subscribe((data: any) => {
+      // TODO remove debugging
       console.log(data);
+      this.initTable(data);
+      this.delegator.txs.staking = data;
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public onTabChange(event) {
+    let tabLabel = event.tab.textLabel.toLowerCase();
+    this.initTable( this.delegator.txs[tabLabel] );
+    // TODO remove debugging 
+    // console.log( tabLabel );
+  }
+
+  public initTable(array) {
+    this.dataSource = new MatTableDataSource<any>([...array]);
+    this.dataSource.paginator = this.paginator;
   }
 
 }
