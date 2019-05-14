208a1211a98f2110b547a76c73f159bcf5c6c22b
aakatev <aakatev@mail.sfsu.edu>
2019-05-14 14:44:51 -0700

    Proposal time period added

--- src/app/components/charts/chart-cards/chart-cards.config.ts
+++ src/app/components/charts/chart-cards/chart-cards.config.ts
@@ -96,16 +96,16 @@ const radarChart = {
   ],
   labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
   colors: [
-    {
-      backgroundColor: 'rgba(105, 0, 132, .2)',
-      borderColor: 'rgba(200, 99, 132, .7)',
-      borderWidth: 2,
-    },
-    {
-      backgroundColor: 'rgba(0, 250, 220, .2)',
-      borderColor: 'rgba(0, 213, 132, .7)',
-      borderWidth: 2,
-    }
+    // {
+    //   backgroundColor: 'rgba(105, 0, 132, .2)',
+    //   borderColor: 'rgba(200, 99, 132, .7)',
+    //   borderWidth: 2,
+    // },
+    // {
+    //   backgroundColor: 'rgba(0, 250, 220, .2)',
+    //   borderColor: 'rgba(0, 213, 132, .7)',
+    //   borderWidth: 2,
+    // }
   ],
   options: {
     responsive: true
