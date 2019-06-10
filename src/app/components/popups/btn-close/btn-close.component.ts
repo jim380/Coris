32ece686f5d8c716f658e47c28d25029b0c59da3
aakatev <aakatev@mail.ccsf.edu>
2019-06-10 12:31:05 -0700

    CSS fixes for desk

--- src/app/components/popups/btn-close/btn-close.component.ts
+++ src/app/components/popups/btn-close/btn-close.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-btn-close',
@@ -10,7 +11,18 @@ export class BtnCloseComponent {
   @Input() dialogRef: MatDialogRef<any>;
   @Input() link: String;
 
+  constructor(
+    private toastr: ToastrService,
+  ){
+
+  }
+
   onCloseClick() {
     this.dialogRef.close();
+  }
+
+
+  onCopySucceess() {
+    this.toastr.success('Copied to clipboard');
   }
 }
