e894c51c93b94ac6812067fe38de35aa8fbcc355
aakatev <aakatev@mail.ccsf.edu>
2019-07-16 01:52:29 -0700

    Typo in deployment script

--- github-deploy.sh
+++ github-deploy.sh
@@ -2,13 +2,11 @@
 repo=explorer-test
 account=aakatev
 
-ng build --prod --build-optimizer --output-path distr --base-href /$repo/
-cd dist
-
-git init
-git remote add origin git@github.com:$aakatev/$repo.git
+# ng build --prod --build-optimizer --output-path distr --base-href /$repo/
+cd distr && git init
+git remote add origin git@github.com:$account/$repo.git
 git add .
 git commit -m 'Deployed to GitHub pages using bash'
 git push origin master -f
 
-cd -
+# cd -
