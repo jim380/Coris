50a2c91ab495355e5b3a5cd46d49ea04e0b23f39
aakatev <aakatev@mail.ccsf.edu>
2019-08-10 07:16:06 -0700

    Netcat ft script added

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Bundling, and deploying (using netcat to transfer files)
+destination_host=0.0.0.0
+tcp_port=8081
+
+ng build --prod --build-optimizer --output-path distr
+cd distr && tar cf - . | nc $destination_host $tcp_port
+
+# Now on recieving, go in served folder and run:
+# nc -l $tcp_port | tar xf -
