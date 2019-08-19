9873a248946cb1a481351a9b08033f7c83c90bb2
aakatev <aakatev@mail.sfsu.edu>
2019-08-19 10:19:15 -0700

    WiP: routing cont.

--- src/app/components/explorer/account/account-detail.component.ts
+++ src/app/components/explorer/account/account-detail.component.ts
@@ -1,7 +1,125 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { PricingService } from 'src/app/services/pricing.service';
-import { TxsService } from 'src/app/services/txs.service';
+// import { Component, OnInit, Inject } from '@angular/core';
+// import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+// import { PricingService } from 'src/app/services/pricing.service';
+// import { TxsService } from 'src/app/services/txs.service';
+
+// @Component({
+//   selector: 'app-account-detail',
+//   templateUrl: './account-detail.component.html',
+//   styleUrls: [
+//     './account-detail.component.scss',
+//   ]
+// })
+// export class AccountDetailComponent implements OnInit {
+//   delegator = {
+//     address: "",
+//     balance: {
+//       total: 0,
+//       available: 0,
+//       delegated: 0,
+//       unbonding: 0,
+//       rewards: 0
+//     },
+//     delegations: null,
+//     redelegations: null,
+//     rewards: null,
+//     unbondingDelegations: null
+//   };
+//   quoteUsd: 0;
+
+//   constructor(
+//     public dialogRef: MatDialogRef<AccountDetailComponent>,
+//     @Inject(MAT_DIALOG_DATA) public data: any, 
+//     private ts: TxsService, 
+//     private ps: PricingService
+//   ) { 
+//     this.delegator.address = data.address;
+//   }
+
+//   ngOnInit() {
+//     this.ts.getAccountInfo(this.delegator.address).subscribe((data: any) => {
+//       // TODO remove debugging
+//       console.log("Account", data);
+//       if(data.value.coins) {
+//         this.delegator.balance.available = Number(data.value.coins[0].amount);
+//         this.delegator.balance.total += Number(data.value.coins[0].amount);
+//         // console.log(this.delegator.balance.total);
+//       }
+//     });
+
+//     this.ts.getDelegations(this.delegator.address).subscribe((data: any) => {
+//       // TODO remove debugging
+//       console.log("Delegations:", data);
+
+//       if(data) {
+//         this.delegator.delegations = data;
+
+//         data.forEach((delegation: any) => {
+//           console.log(Number(delegation.shares));
+//           this.delegator.balance.delegated += Number(delegation.shares);
+//           this.delegator.balance.total += Number(delegation.shares);
+          
+//           // console.log(this.delegator.balance.total);
+//         });
+//       }
+//     });
+
+//     this.ts.getUnbondingDelegations(this.delegator.address).subscribe((data: any) => {
+//       // TODO remove debugging
+//       console.log("Unbonding delegations:", data);
+//       if(data) {
+//         this.delegator.unbondingDelegations = data;
+
+//         data.forEach((delegation: any) => {
+//           delegation.entries.forEach((entry: any) => {
+//             this.delegator.balance.unbonding += Number(entry.balance);
+//             this.delegator.balance.total += Number(entry.balance);    
+//           });
+//         });
+
+//       }
+//     });
+
+//     this.ts.getRedelegations(this.delegator.address).subscribe((data: any) => {
+//       // TODO remove debugging
+//       console.log("Redelegations:", data);
+//       if(data) {
+//         this.delegator.redelegations = data;
+//       }
+//     });
+
+//     this.ts.getDelegatorRewards(this.delegator.address).subscribe((data: any) => {
+//       // TODO remove debugging
+//       console.log("Rewards:", data);
+//       if(data && data.length) {
+//         this.delegator.rewards = data;
+//         data.forEach((reward: any) => {
+//           this.delegator.balance.rewards += Number(reward.amount);
+//           this.delegator.balance.total += Number(reward.amount);
+
+//           console.log(this.delegator.balance.total);
+//         });
+//       }
+//     });
+
+//     this.ps.getAtomPrice()
+//       .subscribe((data: any) => {
+//         // TODO remove debugging
+//         // console.log(data.data['3794']);
+//         this.quoteUsd = data.data['3794'].quote.USD.price;
+//     });
+
+
+//     // this.ts.getDelegatorValidators(this.delegator.address).subscribe((data: any) => {
+//     //   // TODO remove debugging
+//     //   console.log("Validators:", data);
+//     // });
+
+//   }
+
+// }
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-account-detail',
@@ -11,110 +129,21 @@ import { TxsService } from 'src/app/services/txs.service';
   ]
 })
 export class AccountDetailComponent implements OnInit {
-  delegator = {
-    address: "",
-    balance: {
-      total: 0,
-      available: 0,
-      delegated: 0,
-      unbonding: 0,
-      rewards: 0
-    },
-    delegations: null,
-    redelegations: null,
-    rewards: null,
-    unbondingDelegations: null
-  };
-  quoteUsd: 0;
+  private address: String | null = null;
+  private selectedAccount: any | null = null;
+
 
   constructor(
-    public dialogRef: MatDialogRef<AccountDetailComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, 
-    private ts: TxsService, 
-    private ps: PricingService
-  ) { 
-    this.delegator.address = data.address;
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.selectAccount(); 
+    console.log(this.address);
   }
 
-  ngOnInit() {
-    this.ts.getAccountInfo(this.delegator.address).subscribe((data: any) => {
-      // TODO remove debugging
-      console.log("Account", data);
-      if(data.value.coins) {
-        this.delegator.balance.available = Number(data.value.coins[0].amount);
-        this.delegator.balance.total += Number(data.value.coins[0].amount);
-        // console.log(this.delegator.balance.total);
-      }
-    });
-
-    this.ts.getDelegations(this.delegator.address).subscribe((data: any) => {
-      // TODO remove debugging
-      console.log("Delegations:", data);
-
-      if(data) {
-        this.delegator.delegations = data;
-
-        data.forEach((delegation: any) => {
-          console.log(Number(delegation.shares));
-          this.delegator.balance.delegated += Number(delegation.shares);
-          this.delegator.balance.total += Number(delegation.shares);
-          
-          // console.log(this.delegator.balance.total);
-        });
-      }
-    });
-
-    this.ts.getUnbondingDelegations(this.delegator.address).subscribe((data: any) => {
-      // TODO remove debugging
-      console.log("Unbonding delegations:", data);
-      if(data) {
-        this.delegator.unbondingDelegations = data;
-
-        data.forEach((delegation: any) => {
-          delegation.entries.forEach((entry: any) => {
-            this.delegator.balance.unbonding += Number(entry.balance);
-            this.delegator.balance.total += Number(entry.balance);    
-          });
-        });
-
-      }
-    });
-
-    this.ts.getRedelegations(this.delegator.address).subscribe((data: any) => {
-      // TODO remove debugging
-      console.log("Redelegations:", data);
-      if(data) {
-        this.delegator.redelegations = data;
-      }
-    });
-
-    this.ts.getDelegatorRewards(this.delegator.address).subscribe((data: any) => {
-      // TODO remove debugging
-      console.log("Rewards:", data);
-      if(data && data.length) {
-        this.delegator.rewards = data;
-        data.forEach((reward: any) => {
-          this.delegator.balance.rewards += Number(reward.amount);
-          this.delegator.balance.total += Number(reward.amount);
-
-          console.log(this.delegator.balance.total);
-        });
-      }
-    });
-
-    this.ps.getAtomPrice()
-      .subscribe((data: any) => {
-        // TODO remove debugging
-        // console.log(data.data['3794']);
-        this.quoteUsd = data.data['3794'].quote.USD.price;
-    });
-
-
-    // this.ts.getDelegatorValidators(this.delegator.address).subscribe((data: any) => {
-    //   // TODO remove debugging
-    //   console.log("Validators:", data);
-    // });
-
+  selectAccount(): void {
+    this.address = this.route.snapshot.paramMap.get('address');
   }
 
 }
--- src/app/components/explorer/governance/proposal/gov-detail.component.ts
+++ src/app/components/explorer/governance/proposal/gov-detail.component.ts
@@ -1,10 +1,109 @@
-import { Component, OnInit, Inject, QueryList, ViewChildren, AfterViewInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatTableDataSource, MatPaginator, MatDialogRef } from '@angular/material';
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { State } from 'src/app/state';
-import { ValidatorsState } from 'src/app/state/validators/validator.interface';
-import { selectValidatorsState } from 'src/app/state/validators/validators.reducers';
+// import { Component, OnInit, Inject, QueryList, ViewChildren, AfterViewInit } from '@angular/core';
+// import { MAT_DIALOG_DATA, MatTableDataSource, MatPaginator, MatDialogRef } from '@angular/material';
+// import { Observable } from 'rxjs';
+// import { Store } from '@ngrx/store';
+// import { State } from 'src/app/state';
+// import { ValidatorsState } from 'src/app/state/validators/validator.interface';
+// import { selectValidatorsState } from 'src/app/state/validators/validators.reducers';
+
+// @Component({
+//   selector: 'app-gov-detail',
+//   templateUrl: './gov-detail.component.html',
+//   styleUrls: [
+//     './gov-detail.component.scss',
+//   ]
+// })
+// export class GovDetailComponent implements OnInit, AfterViewInit {
+//   public chartType: string = 'pie';
+//   public chartDatasets: Array<any> = [];
+//   public chartLabels: Array<any> = ['Yes', 'No', 'NoWithVeto', 'Abstain'];
+//   public chartColors: Array<any> = [
+//     {
+//       backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1'],
+//       hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5'],
+//       borderWidth: 2,
+//     }
+//   ];
+//   public chartOptions: any = {
+//     responsive: true
+//   };
+
+//   validatorsState: Observable<ValidatorsState>;
+//   votesDataSource = new MatTableDataSource<any>([]);
+//   depositDataSource = new MatTableDataSource<any>([]);
+
+//   votesDisplayedColumns: string[] = ['voter', 'vote'];
+//   depositDisplayedColumns: string[] = ['depositor', 'amount'];
+
+//   @ViewChildren(MatPaginator) paginators = new QueryList<MatPaginator>();
+//   proposal;
+//   constructor(
+//     public dialogRef: MatDialogRef<GovDetailComponent>,
+//     @Inject(MAT_DIALOG_DATA) public data: any, 
+//     private store: Store <State>
+//   ) { 
+//     console.log();
+//     this.proposal = data.proposal;
+//     this.initTable('All');
+//     this.initDepositTable();
+//   }
+
+//   ngOnInit() {
+//     this.validatorsState = this.store.select(selectValidatorsState);
+//     this.initChart();    
+//     // TODO remove debugging
+//     // console.log(this.proposal);
+//   }
+//   ngAfterViewInit() {
+//     this.votesDataSource.paginator = this.paginators.toArray()[0];
+//     this.depositDataSource.paginator = this.paginators.toArray()[1];
+//   }
+//   public initChart() {
+//     let tally = this.proposal.currentTally;
+
+//     this.chartDatasets = [{ 
+//       data: [
+//         tally.yes, 
+//         tally.no, 
+//         tally.no_with_veto, 
+//         tally.abstrain
+//       ], 
+//       label: 'Votes' }
+//     ]
+//   }
+//   public chartClicked(e: any): void { }
+//   public chartHovered(e: any): void { }
+
+//   public initTable(filter) {
+//     this.votesDataSource = new MatTableDataSource<any>([...this.proposal.currentVotes]);
+//     this.votesDataSource.filterPredicate = (data: any, filter: string) => !filter || data.option == filter;
+//     if(filter !== 'All') {
+//       this.votesDataSource.filter = filter;
+//     }
+//     this.votesDataSource.paginator = this.paginators.toArray()[0];
+//   }
+
+//   public onTabChange(event) {
+//     // TODO remove debugging
+//     // console.log(event.tab.textLabel);
+//     this.initTable(event.tab.textLabel);
+//   }
+
+
+//   public initDepositTable() {
+//     // TODO remove debugging
+//     // console.log(this.proposal.currentDeposit);
+//     this.depositDataSource = new MatTableDataSource<any>([...this.proposal.currentDeposit]);
+//     this.depositDataSource.paginator = this.paginators.toArray()[1];
+//   }
+
+//   openAccountDialog(address) {
+//     this.dialogRef.close();
+//     this.data.service.openAccountDialogAddr(address);
+//   }
+// }
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-gov-detail',
@@ -13,92 +112,22 @@ import { selectValidatorsState } from 'src/app/state/validators/validators.reduc
     './gov-detail.component.scss',
   ]
 })
-export class GovDetailComponent implements OnInit, AfterViewInit {
-  public chartType: string = 'pie';
-  public chartDatasets: Array<any> = [];
-  public chartLabels: Array<any> = ['Yes', 'No', 'NoWithVeto', 'Abstain'];
-  public chartColors: Array<any> = [
-    {
-      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1'],
-      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5'],
-      borderWidth: 2,
-    }
-  ];
-  public chartOptions: any = {
-    responsive: true
-  };
+export class GovDetailComponent implements OnInit {
+  private id: String | null = null;
+  private selectedProposal: any | null = null;
 
-  validatorsState: Observable<ValidatorsState>;
-  votesDataSource = new MatTableDataSource<any>([]);
-  depositDataSource = new MatTableDataSource<any>([]);
 
-  votesDisplayedColumns: string[] = ['voter', 'vote'];
-  depositDisplayedColumns: string[] = ['depositor', 'amount'];
-
-  @ViewChildren(MatPaginator) paginators = new QueryList<MatPaginator>();
-  proposal;
   constructor(
-    public dialogRef: MatDialogRef<GovDetailComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, 
-    private store: Store <State>
-  ) { 
-    console.log();
-    this.proposal = data.proposal;
-    this.initTable('All');
-    this.initDepositTable();
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.selectProposal(); 
+    console.log(this.id);
   }
 
-  ngOnInit() {
-    this.validatorsState = this.store.select(selectValidatorsState);
-    this.initChart();    
-    // TODO remove debugging
-    // console.log(this.proposal);
-  }
-  ngAfterViewInit() {
-    this.votesDataSource.paginator = this.paginators.toArray()[0];
-    this.depositDataSource.paginator = this.paginators.toArray()[1];
-  }
-  public initChart() {
-    let tally = this.proposal.currentTally;
-
-    this.chartDatasets = [{ 
-      data: [
-        tally.yes, 
-        tally.no, 
-        tally.no_with_veto, 
-        tally.abstrain
-      ], 
-      label: 'Votes' }
-    ]
-  }
-  public chartClicked(e: any): void { }
-  public chartHovered(e: any): void { }
-
-  public initTable(filter) {
-    this.votesDataSource = new MatTableDataSource<any>([...this.proposal.currentVotes]);
-    this.votesDataSource.filterPredicate = (data: any, filter: string) => !filter || data.option == filter;
-    if(filter !== 'All') {
-      this.votesDataSource.filter = filter;
-    }
-    this.votesDataSource.paginator = this.paginators.toArray()[0];
+  selectProposal(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
   }
 
-  public onTabChange(event) {
-    // TODO remove debugging
-    // console.log(event.tab.textLabel);
-    this.initTable(event.tab.textLabel);
-  }
-
-
-  public initDepositTable() {
-    // TODO remove debugging
-    // console.log(this.proposal.currentDeposit);
-    this.depositDataSource = new MatTableDataSource<any>([...this.proposal.currentDeposit]);
-    this.depositDataSource.paginator = this.paginators.toArray()[1];
-  }
-
-  openAccountDialog(address) {
-    this.dialogRef.close();
-    this.data.service.openAccountDialogAddr(address);
-  }
 }
