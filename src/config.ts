61c77dac494ffb354358d957a93c28b7f3dbdeae
aakatev <aakatev@mail.sfsu.edu>
2019-11-09 16:12:36 -0800

    Enpoints fixed

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // #TODO fix issue with websockets proxy
 const NETWORK = `regen`
 
-export const nodeRpc1 = `https://libration.xyz/${NETWORK}/rpc1`;
-export const nodeRpc2 = `https://libration.xyz/${NETWORK}/rpc2`;
-export const nodeWs = `wss://libration.xyz/${NETWORK}/websocket`;
-export const appApi = `https://libration.xyz/${NETWORK}/api`;
+export const nodeRpc1 = `https://coris.network/${NETWORK}/rpc1`;
+export const nodeRpc2 = `https://coris.network/${NETWORK}/rpc2`;
+export const nodeWs = `wss://coris.network/${NETWORK}/websocket`;
+export const appApi = `https://coris.network/${NETWORK}/api`;
 export const faucetRpc = 'https://regen.coris.network/api'
